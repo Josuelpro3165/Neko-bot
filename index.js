bdc5c6818ab3ec802871be84a122a421871e4d8c
Josuelpro3165 <Josuelproo757@gmail.com>
2026-01-29 04:48:01 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const {
+  default: makeWASocket,
+  useMultiFileAuthState
+} = require("@whiskeysockets/baileys")
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState("auth")
+
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: true
+  })
+
+  sock.ev.on("creds.update", saveCreds)
+
+  sock.ev.on("connection.update", ({ connection }) => {
+    if (connection === "open") {
+      console.log("🐱 Neko bot conectado")
+    }
+  })
+}
+
+startBot()
